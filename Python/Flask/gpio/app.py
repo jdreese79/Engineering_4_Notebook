4498e6f5055b28cc4afb38d6f9611c20fe856e7d
Jack D-H <jdreese79@charlottesvilleschools.org>
2018-10-24 15:46:45 -0400

    Committing these files

--- Python/Flask/gpio/app.py
+++ Python/Flask/gpio/app.py
@@ -12,16 +12,53 @@ app = Flask(__name__)
 @app.route("/", methods=["GET","POST"])
 
 def index():
-	if request.method == "POST":
-		GPIO.output(17,GPIO.HIGH)
-		GPIO.output(18,GPIO.HIGH)
-		msg = request.form.get("submitBtn1")
-		msg = request.form.get("submitBtn2")
+	led1 = GPIO.input(17)
+	led2 = GPIO.input(18)
+	if request.method =="POST":
+		if('hiddenElement1' in request.form);
+			if(led1 == True):
+				GPIO.output(17, GPIO.LOW)
+				msg = "LED 1 is off"
+				checked1 = ""
+			else:
+				GPIO.output(17, GPIO.HIGH)
+				msg1 = request.form.get("submitBtn")
+				checked1 = "checked"
+			if(led2 == True):
+				msg2 = "LED 2 is on"
+				checked2 ="checked'
+			else:
+				msg2 = "LED 2 is off"
+				checked2 = ""
+		else:
+			if(led2 == True):
+				GPIO.output(18, GPIO.LOW)
+				msg2 = "LED 2 is off"
+				checked2 = ""
+			else:
+				GPIO.output(18, GPIO.HIGH)
+				msg2 = request.form.get("submitBtn")
+				checked2 = "checked"
+			if(led1 == True):
+				msg1 = "LED 1 is on"
+				checked1 = "checked"
+			else:
+				msg1 = "LED 1 is off"
+				checked1 = ""
 	else:
-		GPIO.output(17,GPIO.LOW)
-		GPIO.output(18,GPIO.LOW)
-		msg = "No click yet."
-	return render_template("index.html", msg=msg)
+		GPIO.output(17, GPIO.LOW)
+		GPIO.output(18, GPIO.LOW)
+		msg1 = "LED 1 is off"
+		msg2 = "LED 2 is off"
+		checked1 = ""
+		checked2 = ""
+	return render_template(
+		"index.html",
+		msg1=msg1,
+		msg2=msg2,
+		checked1=checked1,
+		checked2=checked2)
+
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=80)
